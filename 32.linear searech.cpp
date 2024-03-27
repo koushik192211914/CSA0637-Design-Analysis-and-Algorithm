62282e4a3e63281b77bb3c16b398e62ec823ebb4
koushik192211914 <141816735+koushik192211914@users.noreply.github.com>
2024-03-27 21:53:17 +0530

    Update 32.linear searech.cpp

--- 32.linear searech.cpp
+++ 32.linear searech.cpp
@@ -1,10 +1,6 @@
-// C program to implement linear search using loops
 #include <stdio.h>
-
-// linear search function that searches the key in arr
 int linearSearch(int* arr, int size, int key)
 {
-	// starting traversal
 	for (int i = 0; i < size; i++) {
 		// checking condition
 		if (arr[i] == key) {
@@ -14,18 +10,12 @@ int linearSearch(int* arr, int size, int key)
 	return -1;
 }
 
-// Driver code
 int main()
 {
 	int arr[10] = { 3, 4, 1, 7, 5, 8, 11, 42, 3, 13 };
 	int size = sizeof(arr) / sizeof(arr[0]);
 	int key = 4;
-
-	// calling linearSearch
 	int index = linearSearch(arr, size, key);
-
-	// printing result based on value returned by
-	// linearSearch()
 	if (index == -1) {
 		printf("The element is not present in the arr.");
 	}
